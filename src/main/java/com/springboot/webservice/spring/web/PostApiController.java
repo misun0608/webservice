6e5224c9ad16ce712a2eeaaacc08ea7575036ea5
Misun Lim <chicmsl@gmail.com>
2021-10-01 12:06:20 +0900

    feat: mustache 추가

---
+++ src/main/java/com/springboot/webservice/spring/web/PostApiController.java
@@ -0,0 +1,30 @@
+package com.springboot.webservice.spring.web;
+
+import com.springboot.webservice.spring.service.posts.PostsService;
+import com.springboot.webservice.spring.web.dto.PostsResponseDto;
+import com.springboot.webservice.spring.web.dto.PostsSaveRequestDto;
+import com.springboot.webservice.spring.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
---
+++ src/main/java/com/springboot/webservice/spring/web/dto/PostsResponseDto.java
@@ -0,0 +1,22 @@
+package com.springboot.webservice.spring.web.dto;
+
+import com.springboot.webservice.spring.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+
+}
